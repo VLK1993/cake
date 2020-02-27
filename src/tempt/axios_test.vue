53ecffa93d8e6e30f456fb5b8d48fcbde895c5ab
Khoa <hakubawhite@gmail.com>
2020-02-27 16:18:56 +0700

    Update axios_test.vue

--- src/tempt/axios_test.vue
+++ src/tempt/axios_test.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div id="app-todo">
     <h1>Todos</h1>
       <ul>
         <li v-for="todo of todos" :key="todo.id">
@@ -11,15 +11,27 @@
 </template>
 
 <script>
-//import axios from 'axios';
+import axios from 'axios';
 
 //import todos from '/json/todos.json';
 
 export default {
-  name:'app',
+  name:'app-todo',
   data(){
     return{
-      todos:[]
+      todos:[
+        
+      ]
+    }
+  },
+  async created(){
+    try{
+      const res = await axios.get('https://vlk1993.github.io/cake/dist/json/todos.json');
+      this.todos = res.data;
+
+    }
+    catch(e){
+      console.error(e);
     }
   }
 
