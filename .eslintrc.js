42cb87efafafff9ca1505ad58da21367e36b6642
Dramiel Leayal <leayal@users.noreply.github.com>
2020-04-06 12:12:44 +0700

    Fix the filtering

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,26 @@
 module.exports = {
   root: true,
+
   env: {
     node: true,
     browser: true
   },
+
   extends: ["plugin:vue/essential", "@vue/prettier"],
+
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    'no-console': 'off',
+    'no-debugger': 'off',
+    'vue/no-unused-components': 'warning',
+    'vue/no-unused-vars': 'warning'
   },
+
   parserOptions: {
-    parser: "babel-eslint"
-  }
+    parser: 'babel-eslint'
+  },
+
+  'extends': [
+    'plugin:vue/essential',
+    '@vue/prettier'
+  ]
 };
